bad9bee69a1fa68bc788a9180d6c29ee40098dc7
egstar <borham@hotmail.com>
2023-03-16 14:33:16 +0200

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
-*
-* main - function description
-*
-*/
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

2a974c3ff071c09752b28968ac127b8e66a54bda
egstar <borham@hotmail.com>
2023-03-16 14:18:56 +0200

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
 *
-* main -function description
+* main - function description
 *
 */
-int main(void) // Main  function
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

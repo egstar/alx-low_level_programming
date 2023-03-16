80d26b0569fb8f1cc96503b57ec7e75ab0b8f087
egstar <borham@hotmail.com>
2023-03-16 18:04:32 +0200

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
 /**
-	* Main function 
-	* Printing each type's size
+	* main function
+	* Printing each variable size
 	* Return (0)
+	* Print variables size for 32bit x 64bit:
+	*	char
+	*	int
+	*	long int
+	*	long long int
+	*	float
 	*/
 int main(void)
 {
@@ -12,11 +17,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	printf("Size of char: %lu byte(s)\n", sizeof(a));
-	printf("Size of int: %lu byte(s)\n", sizeof(b));
-	printf("Size of long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of float: %lu byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
-

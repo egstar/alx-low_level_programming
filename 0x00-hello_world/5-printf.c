e496399ff0733aae4997f783b4ed67b1aac573d6
egstar <borham@hotmail.com>
2023-03-16 15:46:28 +0200

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
 	*
 	* Return: 0 -> Success
 	*/
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }

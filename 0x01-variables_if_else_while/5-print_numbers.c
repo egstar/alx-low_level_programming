97058131889b58d2a175b229924027817dbcf679
egstar <borham@hotmail.com>
2023-03-18 01:58:45 +0200

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /*
-* main - Function description
-*
-* Return: (0) always success
-*/
+	* main - Function description
+	*
+	* Return: (0) always success
+	*/
 int main(void)
 {
 	int n = 0;
+
 	while (n < 10)
 	{
 		printf("%d", n);

02eb891d576ec5480d62be04a351ae84a0d495a4
egstar <borham@hotmail.com>
2023-03-18 03:14:12 +0200

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,15 @@
 	*/
 int main(void)
 {
-	int n,i;
+	int n, i;
+
 	for (n = 0; n < 10 ; n++)
 	{
 		for (i = n + 1; i < 10; i++)
 		{
 			putchar(n + '0');
 			putchar(i + '0');
-			if(n != 8 || i != 9)
+			if (n != 8 || i != 9)
 			{
 				putchar(',');
 				putchar(' ');

8487df97927e1e0ad2be6d29dc32045f3037d2ee
egstar <borham@hotmail.com>
2023-03-22 03:38:03 +0200

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include"main.h"
 /**
 	* _islower - Function description
-	*
-	* 
+	* @c - description
+	*  Return: (1) or (0)
 	*/
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
 		return (1);
-	} else {
+	}
+	else
+	{
 		return (0);
 	}
 }

18efa4fe13ab6f3f6fa4487ce62790c9d9a557e6
egstar <borham@hotmail.com>
2023-03-22 02:37:21 +0200

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <main.h>
 /**
 	* main - Function description
 	*
 	* Return: (0) always success
 	*/
-int main(void)
+ int main(void)
 {
 	char c[] = "_putchar";
+	int i;
 
-	printf('%s', c);
+	for (i = 0; i<9; i++)
+	{
+		putchar(c[i]);
+		if (c[i] == '\0')
+		{
+			putchar('\n');
+		}
+	}
 
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 
 - Task 0
 	- Print _putchar string 
+

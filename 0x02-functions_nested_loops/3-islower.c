11d7da192fd9b6ffbf34482c8e9272c76cfb8704
egstar <borham@hotmail.com>
2023-03-22 03:32:55 +0200

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#includemain.h
+/**
+	* _islower - Function description
+	*
+	* 
+	*/
+int _islower(int c)
+{
+	if (c > 126 && c < 123)
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,6 @@
 
 - Task 2
 	- Loop for alphabets from 'a' to 'z' x 10 times
+
+- Task 3
+	- If statement with ascii codes

cf7a0738dbf58f5b7ce690f5f5fc5a6818212a5f
egstar <borham@hotmail.com>
2023-03-22 03:26:48 +0200

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,11 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (s = 'a'; s <= 'z'; s++)
+		s= 'a';
+		while(s <= 'z')
 		{
 			_putchar(s);
+			s++;
 		}
 		_putchar('\n');
 	}
